9df25fb25cb967c49ce129774c9354c661c9adc5
IgorDv5 <igoraluno2005@gmail.com>
2025-06-21 19:36:30 -0300

    Repositories Implementados

---
+++ src/main/java/com/example/loja/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.example.loja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.loja.domain.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+}
