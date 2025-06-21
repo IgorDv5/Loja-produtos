541cba060f19c245c6c0dc971c77088053c12eb8
IgorDv5 <igoraluno2005@gmail.com>
2025-06-21 18:47:36 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/example/loja/domain/Produto.java
@@ -0,0 +1,90 @@
+package com.example.loja.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Entity
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	private double preco;
+	private String Marca;
+	private Integer tamanho;
+	@Lob
+	private byte[] foto;
+	
+	public Produto() {
+		super();
+	}
+
+	public Produto(Integer id, String nome, double preco, String marca, Integer tamanho, byte[] foto) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		Marca = marca;
+		this.tamanho = tamanho;
+		this.foto = foto;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getMarca() {
+		return Marca;
+	}
+
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+
+	public Integer getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(Integer tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public byte[] getFoto() {
+		return foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+
+	
+
+}
